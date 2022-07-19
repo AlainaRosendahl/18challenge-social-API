4a08e823d5fe88342b554a06ce2db2e8b843feed
Alaina Rosendahl <elenarosendahl16@gmail.com>
2022-07-19 18:55:35 -0500

    module error thoughs

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -39,7 +39,7 @@ const ThoughtsSchema = new Schema(
     createdAt: {
         type: Date,
         default: Date.now,
-        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        get: createdAtVal => dateFormat(createdAtVal)
     },
     username: {
         type: String,

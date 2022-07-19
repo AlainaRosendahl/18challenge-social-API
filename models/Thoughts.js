1403a39deb1ba8a28c36faa080e00e59a1d3496d
Alaina Rosendahl <elenarosendahl16@gmail.com>
2022-07-19 18:59:47 -0500

    debugging syntax err

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,5 +1,5 @@
 const { Schema, model, Types } = require('mongoose');
-const moment = require('moment');
+const dateFormat = require('../utils/dateFormat');
 
 const ReactionsSchema = new Schema(
     {
@@ -19,7 +19,7 @@ const ReactionsSchema = new Schema(
     createdAt: {
         type: Date,
         default: Date.now,
-        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        get: createdAtVal => dateFormat(createdAtVal)
     }
     },
     {
--- routes/api/index.js
+++ routes/api/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const usersRoutes = require('./user-routes');
 const thoughtsRoutes = require('./thought-routes');
 
-router.use('/users', usersRoutes);
-router.use('/thoughts', thoughtsRoutes);
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
